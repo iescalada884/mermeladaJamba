aa51ca4a0db69b0e1df61be86030b35a643bab8d
iescalada884 <ied884@alumnos.unican.es>
2024-07-10 23:23:45 +0200

    Poryecto Inicial Unity =)

---
+++ Poti/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Add your game management methods here
+    public void changeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
